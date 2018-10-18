71f77f9dfc282b2eb82ee372024b1c89d0625661
Wil Cook <wil.cook.is@gmail.com>
2018-10-18 17:57:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var chocolateBars = new Array (
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  );
+  
+  function addElementToBeginningOfArray (a, b) {
+    newArray = a.unshift(b);
+    return newArray;
+  }
+  
+  addElementToBeginningOfArray(chocolateBars, "banana");
+  console.log(chocolateBars);
+  console.log(newArray);
+  
+  function destructivelyAddElementToBeginningOfArray (a, b) {
+    a.unshift(b);
+    return a;
+  }
+  
+  destructivelyAddElementToBeginningOfArray(chocolateBars, "banana");
+  console.log(chocolateBars);
+  
+  function addElementToEndOfArray (a, b) {
+    a.push(b);
+  }
+  
+  function accessElementInArray (a, b) {
+    return a[b];
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray (a) {
+    a.shift();
+    return a;
+  }
+  
+  function removeElementFromBeginningOfArray (a) {
+    return a.slice(1);
+  }
+  
+  function destructivelyRemoveElementFromEndOfArray (a) {
+    a = a.pop();
+    return a;
+  }
+  
+  function removeElementFromEndOfArray(a) {
+    return a.slice(0, a.length - 1);
+  }
